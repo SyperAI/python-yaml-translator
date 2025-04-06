136014267ce4bc0286bcea4f575763a241ab1f70
StarMan <alexkom041206@gmail.com>
2025-04-06 17:34:09 +0200

    url updated

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='Class for working with translations based on YAML files.',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/my_library',
+    url='https://github.com/SyperAI/python-yaml-translator',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
