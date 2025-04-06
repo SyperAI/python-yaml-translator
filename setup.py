b799cba91b195fcdcf4bbffa1f1593f57927320c
StarMan <alexkom041206@gmail.com>
2025-04-06 17:35:31 +0200

    install_requires value updated

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt", "r", encoding="utf-8") as f:
-    requirements = f.read().splitlines()
-
 setup(
     name='YAML Translator',
     version='1.0.0',
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=['PyYAML==6.0.2'],
     author='StarMan',
     description='Class for working with translations based on YAML files.',
     long_description=open('README.md', encoding='utf-8').read(),
