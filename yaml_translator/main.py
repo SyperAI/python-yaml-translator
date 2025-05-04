fbc8805080611588ddee9bf7665040a8d5b3019c
StarMan <alexkom041206@gmail.com>
2025-05-05 01:34:49 +0200

    Added ability to format translated values

--- yaml_translator/main.py
+++ yaml_translator/main.py
@@ -1,16 +1,47 @@
 import typing
 from pathlib import Path
-from typing import Tuple, Any
+from typing import Any
 
 import yaml
 
-def dict_depth(d):
+
+def dict_depth(d: dict):
+    """
+    This function allows you to find the maximum depth of a given dictionary. Depth is the number of invested dictionaries.
+
+    :param d: Any dictionary deep of which you need to find
+    :return: Given dictionary depth
+    """
     if not isinstance(d, dict) or not d:
         return 0
     return 1 + max((dict_depth(v) for v in d.values() if isinstance(v, dict)), default=0)
 
 
+def _format(dict_data: dict, format_values: dict) -> dict:
+    """
+    This function allows formatting on the specified data key in the dictionary with support of invested dictionaries.
+
+    :param dict_data: Dictionary, Data in which you need to format.
+    :param format_values: The keys to which formatting will occur and values that will be set in the form of a dictionary.
+    :return: Given dictionary after formatting
+    """
+    if format_values is None:
+        return dict_data
+
+    result = {}
+    for key, value in dict_data.items():
+        if isinstance(value, dict):
+            result[key] = _format(value, format_values)
+        elif isinstance(value, str):
+            result[key] = value.format(**format_values)
+    return result
+
+
 class DeepDict(dict):
+    """
+    Improved dictionary with support for obtaining data from invested dictionaries in 1 request
+    """
+
     def __init__(self, dict_):
         super().__init__(dict_)
 
@@ -29,14 +60,31 @@ class DeepDict(dict):
                 item = item.__getitem__(key)
         return item
 
-    def get(self, key, default=None):
+    def get(self, key, default=None, format: dict = None) -> Any:
+        """
+
+        :param key:
+        :param default: Value that will be returned if key is not in dict
+        :param format: Pass if you want to format some values in dictionary. Must be dict where keys is format keys and
+         values is values which will be replaced by the specified keys.
+          The principle of operation is the same as the built-in .format() func of the str class
+        :return:
+        """
+
         try:
-            return self[key]
+            if format is None:
+                return self[key]
+            else:
+                return _format(self, format)
         except KeyError:
             return default
 
 
 class Translation:
+    """
+
+    """
+
     _translations: dict[str, DeepDict] = {}
 
     def __repr__(self):
@@ -44,18 +92,25 @@ class Translation:
 
     def __init__(self, path: Path) -> None:
         for translation in path.iterdir():
-            if translation.is_file() and translation.suffix == '.yaml':
+            if translation.is_file() and translation.suffix == '.yaml' or translation.suffix == '.yml':
                 with open(translation, 'r', encoding='utf-8') as f:
                     self._translations[translation.stem] = DeepDict(yaml.safe_load(f))
 
-    def get(self, path: str, language) -> typing.Any:
-        return self._translations[language].get(path)
+    def get(self, path: str, language: str, format: dict = None) -> typing.Any:
+        """
 
-    def get_all(self, path: str) -> dict:
+        :param path:
+        :param language:
+        :param format:
+        :return:
+        """
+        return self._translations[language].get(path, format=format)
+
+    def get_all(self, path: str, format: dict = None) -> dict:
         translations = {}
 
         for language in self._translations.keys():
-            translations[language] = self.get(path, language)
+            translations[language] = self.get(path, language, format)
 
         return translations
 
@@ -76,12 +131,7 @@ class Translator:
 
 
 if __name__ == "__main__":
-    translator = Translator()
+    translator = Translator('../translations')
 
-    test_translation = translator.get("test")
-
-    print(test_translation.get("post_removed_owner", "en"))
-    print(test_translation.get_all("post_removed_owner"))
-    print(translator.groups())
-
-
+    print(translator.get('test').get('test', 'ru', {'foo': 123}))
+    print(translator.get('test').get_all('test', {'foo': 123}))
