90fa926ce575c7b5e0f15a5afb42086e90c354d4
StarMan <alexkom041206@gmail.com>
2025-05-05 02:45:19 +0200

    Version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='YAML Translator',
-    version='1.1.0',
+    version='1.2.0',
     packages=find_packages(),
     install_requires=['PyYAML==6.0.2'],
     author='StarMan',
